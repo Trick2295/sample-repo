f8e57b32445cbec655c9572e2c5953dc2f57243a
Patrick Justin Ariado <patrickjustin_ariado@tup.edu.ph>
2025-03-10 19:27:45 +0800

    Final Feature Change eme

---
+++ script.js
@@ -0,0 +1,19 @@
+sdfsdfsdf
+
+let audio = document.getElementById("audio");
+
+function playPause() {
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+    }
+}
+
+function nextSong() {
+    alert("Next song feature coming soon!");
+}
+
+function prevSong() {
+    alert("Previous song feature coming soon!");
+}
